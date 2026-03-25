3997e82100ea23dc367380dab28c84d9711047cf
Misael De Jesus <dejesusmisael492@gmail.com>
2026-03-25 16:55:53 +0000

    🔧Chore: Vit  configuration En progeso.

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// importo la duncion de configuracion de Vite
+import{ defineConfig} from 'vite'
+//importo un resolvedor de rutas
+import { resolve} from "node:path"
+
+//exportar una instancia de configuracion 
+export default defineConfig({})
